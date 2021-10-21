759e0d2ddb213e6dda88f40752f466f3dbe473ac
Daman Arora <damanarora724@gmail.com>
2021-10-21 19:10:19 +0530

    Checking var

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
+    @Value("${server.port}")
+    private String port;
+
+
+
     @GetMapping("/")
     public String index(){
+        System.out.println(port);
+        System.out.println(System.getenv("message"));
         return "Hello world";
     }
 
